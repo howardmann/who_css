9ba7fe5761b4c38cbb18c214465282b34649f220
howardmann <howardmann27@gmail.com>
2017-02-15 22:48:34 +1000

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  var $button = $('button');
+
+  $button.on('click', function(){
+    $(this).toggleClass('active');
+
+    var $btnClosest = $(this).siblings('button');
+    if ($btnClosest.hasClass('active')){
+      $btnClosest.toggleClass('active');
+    }
+
+  })
+});
